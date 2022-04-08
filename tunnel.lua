12735fd5f6d4cc8bd22bb19465d2f9a9572babbf
musergi <sergi.mus@gmail.com>
2022-04-08 11:51:47 +0200

    Solved bug for odd height

--- tunnel.lua
+++ tunnel.lua
@@ -2,32 +2,46 @@ width = tonumber(arg[1])
 height = tonumber(arg[2])
 depth = tonumber(arg[3])
 
+
+dir_x = 1 -- 1: right, -1: left
+dir_y = 1 -- 1: up, -1: down
+
 remaining_depth = depth
 while remaining_depth > 0 do
 	turtle.dig()
 	turtle.forward()
-	turtle.turnRight()
-	remaining_height = height
-	while remaining_height > 0 do
-		for i=1,width-1 do
-			turtle.dig()
-			turtle.forward()
-		end
-		if remaining_height ~= 1 then
-			turtle.turnLeft()
-			turtle.turnLeft()
-			turtle.digUp()
-			turtle.up()
-		end
-		remaining_height = remaining_height - 1
-	end
-	if height % 2 == 0 then
+	
+	if dir_x == 1 then
 		turtle.turnRight()
 	else
 		turtle.turnLeft()
 	end
-	for i=1,height-1 do
-		turtle.down()
+
+	y_advance = 0
+	while y_advance < height-1 do
+		for i=1,width-1 do
+			turtle.dig()
+			turtle.forward()
+		end
+		turtle.turnLeft()
+		turtle.turnLeft()
+		dir_x = -dir_x
+		if dir_y == 1 then
+			turtle.digUp()
+			turtle.up()
+		else
+			turtle.digDown()
+			turtle.down()
+		end
+		y_advance = y_advance + 1
 	end
+	dir_y = -dir_y
+
+	if dir_x == 1 then
+		turtle.turnLeft()
+	else
+		turtle.turnRight()
+	end
+
 	remaining_depth = remaining_depth - 1
 end
